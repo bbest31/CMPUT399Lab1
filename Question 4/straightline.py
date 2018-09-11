65e09060ae218755fca7f3580462d4896f411b59
bbest31 <brandonbest94@live.ca>
2018-09-10 23:32:12 -0400

    Straight line done

--- Question 4/straightline.py
+++ Question 4/straightline.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # Make robot move in a straight line
+
+from time import sleep
+from ev3dev.ev3 import *
+
+motorRight = LargeMotor(OUTPUT_C)
+motorLeft = LargeMotor(OUTPUT_B)
+
+assert motorRight.connected
+assert motorLeft.connected
+
+Leds.all_off()
+
+motorRight.run_timed(speed_sp=700,time_sp=700)
+motorLeft.run_timed(speed_sp=700,time_sp=700)
+
