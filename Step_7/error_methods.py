68246f68e822bc1004ae495a3d411acec0cf214f
Pineros Rhon <pierosrh@registrar.ualberta.ca>
2018-09-23 15:40:04 -0600

    calculations for straight line and rotation errors for step 7.

--- Step_7/error_methods.py
+++ Step_7/error_methods.py
@@ -15,48 +15,87 @@ gy = GyroSensor()
 # Put the gyro sensor into ANGLE mode.
 gy.mode = 'GYRO-ANG'
 
+#-------------------------
+#Constants
+
+#In meters
+wheelDiameter = 5.5/100
+#Degrees per second for the wheels
+degreesPerSecond = 180
+#Wheel circumference in meters
+wheelCircumference = wheelDiameter*pi
+#Speed for the wheels, in meters per second
+wheelVelocity = ((wheelCircumference)*(degreesPerSecond/360))
+#-------------------------
+
 assert motorRight.connected
 assert motorLeft.connected
 
 Leds.set_color(Leds.LEFT,  Leds.ORANGE)
 sleep(0.5)
 
-# Straight line movement
 
-
-def straightLine(speed, sec):
-    startAngle = gy.value()
-    motorRight.run_timed(time_sp=sec * 1000, speed_sp=speed)
-    motorLeft.run_timed(time_sp=sec * 1000, speed_sp=speed)
-    sleep(sec+1)
-    print("Angle Deviation = " + str(abs(startAngle-gy.value)))
-
-
-straightLine(250, 4)
-
-
-# Rotating Movement
-
-
-rotation(20)
-
-
-
-#distance:float. Distance that we want the robot to travel in meters
+#distance:float. Distance that we want the robot to travel in meters (must be positive)
 #returns: float. The error, defined by the actual distance (according to the enconders)
 #                and the expected distance
 def straightLineError(distance):
-    return 0
+    #In seconds
+    timeOfMovement = (x/wheelVelocity)
+    #Sensor Data
+    #Current reading of the tachometer of each wheel  (in degrees)
+    startingTachoReadingLeft = motorLeft.position
+    startingTachoReadingRight = motorRight.position
+
+    motorRight.run_timed(time_sp=timeOfMovement * 1000, speed_sp=180)
+    motorLeft.run_timed(time_sp=timeOfMovement * 1000, speed_sp=180)
+    
+    sleep(timeOfMovement + 0.5)
+    #Sensor Data
+    #Final reading for the tachometer of each wheel
+    finalTachoReadingLeft = motorLeft.position
+    finalTachoReadingRight = motorRight.position 
+
+    #Get the delta for the tachometer of each wheel, this will give us the actual
+    #number of degrees that each wheel rotated
+    tachoDeltaLeft =  finalTachoReadingLeft - startingTachoReadingLeft
+    tachoDeltaRight = finalTachoReadingRight - startingTachoReadingRight
+
+    #We use a simple rule of 3 to calculate the distance travelled by each wheel
+    #This distance is in meters
+    distanceLeft = (tachoDeltaLeft*wheelCircumference)/360
+    distanceRight = (tachoDeltaRight*wheelCircumference)/360
+
+    #Get the average of the two distance traversed by each wheel. Getting this average
+    #should give us the distance of the midpoint of the two weels a.k.a the center of the axle
+    distanceTraversed = (distanceLeft + distanceRight)/2
+
+    print("The distance entered was: " + str(distance) + " meters")
+    print("The distance as calculated by using the tachometer was: " + str(distanceTraversed) + " meters")
+    print("The absolute error is " + str(distance - distanceTraversed))
+    print("The relative error is "+ str((distance - distanceTraversed)/distanceTraversed))
 
 
-#angle:float. The angle we want the robot to rotate (in degrees)
+#angle:float. The angle we want the robot to rotate (in degrees, must be positive)
 #returns: float. The error, defined by the actual distance (according to the enconders)
 #                and the expected distance
 def rotationError(angle):
 
     startAngle = gy.value()
-    motorRight.run_timed(time_sp=10 * 1000, speed_sp=180)
-    motorLeft.run_timed(time_sp=10 * 1000, speed_sp=-180)
-    sleep(10)
-    print('Angle deviation = '+str(abs(startAngle-gy.value())))
-    return 0
+    #Total distance that the robot has to "traverse", represented the longitude the arc of the
+    #circle formed when the robot rotates in place by "angle" degrees 
+    arcLength = (degrees/360)*wheelCircumference
+    
+    #In seconds
+    timeOfMovement = (arcLength/wheelVelocity)
+
+    motorRight.run_timed(time_sp=timeOfMovement * 1000, speed_sp=180)
+    motorLeft.run_timed(time_sp=timeOfMovement * 1000, speed_sp=-180)
+    sleep(timeOfMovement + 0.5)
+
+    endAngle = gy.value()
+    angleDelta = endAngle - startAngle
+
+    print("The angle entered was: " + str(angle) + " degrees")
+    print("The distance as calculated by using the gyroscope was: " + str(angleDelta) + " meters")
+    print("The absolute error is " + str(angle - angleDelta))
+    print("The relative error is "+ str((angle - angleDelta)/angleDelta))
