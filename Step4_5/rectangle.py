9615d2b7aa3022fbfc76c11ad29e70c186b163db
Pineros Rhon <pierosrh@registrar.ualberta.ca>
2018-09-23 19:53:55 -0600

    added new method to rotate vehicle

--- Step4_5/rectangle.py
+++ Step4_5/rectangle.py
@@ -15,10 +15,42 @@ gy.mode='GYRO-ANG'
 units = gy.units
 #reports 'deg' meaning degrees
 
+def rotation(angle):
+    intialValue = gy.value()
+    #In meters
+    robotAxle = 10.7/100
+    #The circle that the robots draws when rotating in place
+    circumference = (robotAxle*pi)
+    #Total distance that the robot has to "traverse", represented the longitude the arc of the
+    #circle formed when the robot rotates in place by "angle" degrees 
+
+    #We use pi because wheels are rotating 90 degrees per second
+    #In meters per second
+    velocityOfLeftWheel = (3*pi/2)*(wheelDiameter/2)
+    velocityOfRightWheel = -(3*pi/2)*(wheelDiameter/2)
+
+    #Angular velocity of vehicle in radians per second
+    angularVelocityOfVehicle = abs(velocityOfRightWheel - velocityOfLeftWheel)/robotAxle
+
+    #We convert the angle to radians
+    angleInRadians = (angle*2*pi)/360
+
+    #We use a rule of 3 to figure out how long should It take to get to the given angle
+    #with the angularVelocityOfVehicle
+    # this in seconds
+    timeOfMovement = angleInRadians/angularVelocityOfVehicle
+
+    motorRight.run_timed(time_sp=int(timeOfMovement * 1000), speed_sp=-270)
+    motorLeft.run_timed(time_sp=int(timeOfMovement * 1000), speed_sp=270)
+    sleep(timeOfMovement/1000 + 0.5)
+
+
+
+
 def turnRight():
     gy = GyroSensor() 
     gy.mode='GYRO-ANG'
-    
+
     firstReading = gy.angle
     sleep(0.5)
     currentGyroAngle = 0
@@ -68,13 +100,13 @@ numberOfRectangles=3
 for i in range(0,numberOfRectangles):
     #Rectangle
     runHeight()
-    turnRight()
+    rotation(90)
     runWidth()
-    turnRight()
+    rotation(90)
     runHeight()
-    turnRight()
+    rotation(90)
     runWidth()
-    turnRight()
+    rotation(90)
     sleep(0.5)
 
 
