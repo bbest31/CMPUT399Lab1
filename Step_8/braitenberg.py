23907f94a250ad8d2bcc740e693d2e53467bcc56
bbest31 <brandonbest94@live.ca>
2018-09-23 18:44:57 -0600

    Braitenberg done

--- Step_8/braitenberg.py
+++ Step_8/braitenberg.py
@@ -11,9 +11,6 @@ motorLeft = LargeMotor(OUTPUT_B)
 rightSensor = ColorSensor(INPUT_4)
 leftSensor = ColorSensor(INPUT_2)
 
-ultrasonicSensor = UltrasonicSensor()
-
-
 # Put the color sensors into AMBIENT mode.
 leftSensor.mode = 'COL-AMBIENT'
 rightSensor.mode = 'COL-AMBIENT'
@@ -61,7 +58,9 @@ def agressive(thresh):
             motorLeft.run_forever(speed_sp=280)
             motorRight.run_forever(speed_sp=280)
 
-#TODO we should check if we should change this to recompute the best intensity
+# TODO we should check if we should change this to recompute the best intensity
+
+
 def love(thresh):
     motorLeft.run_forever(speed_sp=280)
     motorRight.run_forever(speed_sp=280)
@@ -70,22 +69,23 @@ def love(thresh):
     sensorDelta = 0
     while(True):
         if(rightSensor.ambient_light_intensity > thresh or leftSensor.ambient_light_intensity > thresh):
-            #if this value is 0, the reading is the same for both sensors
+            # if this value is 0, the reading is the same for both sensors
             #   if it is less than 0 then the light is more intense for the left sensor
             #   if it is greater than 0 then the light is more intense for the right sensor
-            sensorDelta = rightSensor.ambient_light_intensity - leftSensor.ambient_light_intensity
-            
-            #Smoother turns when delta is small
+            sensorDelta = rightSensor.ambient_light_intensity - \
+                leftSensor.ambient_light_intensity
+
+            # Smoother turns when delta is small
             if (abs(sensorDelta) <= 2):
                 speedModifier = 15
             else:
                 speedModifier = 1
 
-            #Light is more intense for the rightSensor
+            # Light is more intense for the rightSensor
             if (sensorDelta > 1):
                 motorLeft.run_forever(speed_sp=280)
                 motorRight.run_forever(speed_sp=10*speedModifier)
-            #Light is more intense for the leftSensor
+            # Light is more intense for the leftSensor
             elif (sensorDelta < -1):
                 motorLeft.run_forever(speed_sp=10*speedModifier)
                 motorRight.run_forever(speed_sp=280)
@@ -93,66 +93,49 @@ def love(thresh):
                 motorLeft.run_forever(speed_sp=280)
                 motorRight.run_forever(speed_sp=280)
 
-            if ((rightSensor.ambient_light_intensity >= thresh*3  or leftSensor.ambient_light_intensity >= thresh*3) and abs(sensorDelta) <= 2):
+            if ((rightSensor.ambient_light_intensity >= thresh*5 or leftSensor.ambient_light_intensity >= thresh*5) and abs(sensorDelta) <= 2):
                 break
+            # if(ultrasonicSensor.distance_centimeters < 4):
+            #   break
             previousSensorDelta = sensorDelta
-        
+
         motorLeft.run_forever(speed_sp=280)
         motorRight.run_forever(speed_sp=280)
 
-#Seems to work, most of the time
+
+def adjust(i):
+    if(i == 0):
+        motorRight.run_forever(speed_sp=200)
+        motorLeft.run_forever(speed_sp=280)
+        
+    else:
+        motorRight.run_forever(speed_sp=280)
+        motorLeft.run_forever(speed_sp=200)
+
 def explorer(thresh):
     motorLeft.run_forever(speed_sp=280)
     motorRight.run_forever(speed_sp=280)
-    speedModifier = 1
-    previousSensorDelta = 0
-    sensorDelta = 0
-    bestIntensity = 0
+    i = 0
+    bestIntensity = thresh
     while(True):
         if(rightSensor.ambient_light_intensity > thresh or leftSensor.ambient_light_intensity > thresh):
-            bestIntensity = max(bestIntensity, min(rightSensor.ambient_light_intensity,leftSensor.ambient_light_intensity))
-            #if this value is 0, the reading is the same for both sensors
-            #   if it is less than 0 then the light is more intense for the left sensor
-            #   if it is greater than 0 then the light is more intense for the right sensor
-            sensorDelta = rightSensor.ambient_light_intensity - leftSensor.ambient_light_intensity
-            
-            #Smoother turns when delta is small
-            if (abs(sensorDelta) <= 2):
-                speedModifier = 15
+            bestIntensity = max(bestIntensity, max(
+                rightSensor.ambient_light_intensity, leftSensor.ambient_light_intensity))
+            if(rightSensor.ambient_light_intensity > leftSensor.ambient_light_intensity):
+                i = 0
             else:
-                speedModifier = 1
-
-            #Light is more intense for the rightSensor
-            if (sensorDelta > 1):
-                motorLeft.run_forever(speed_sp=280)
-                motorRight.run_forever(speed_sp=10*speedModifier)
-            #Light is more intense for the leftSensor
-            elif (sensorDelta < -1):
-                motorLeft.run_forever(speed_sp=10*speedModifier)
-                motorRight.run_forever(speed_sp=280)
-            else:
-                motorLeft.run_forever(speed_sp=280)
-                motorRight.run_forever(speed_sp=280)
-            if ((rightSensor.ambient_light_intensity >= bestIntensity  or leftSensor.ambient_light_intensity >= bestIntensity) and abs(sensorDelta) <= 3):
-                print(bestIntensity)
-                while min(rightSensor.ambient_light_intensity,leftSensor.ambient_light_intensity) <= (bestIntensity+1):
-                    motorLeft.stop()
-                    motorRight.stop()    
-                bestIntensity = min(rightSensor.ambient_light_intensity,leftSensor.ambient_light_intensity)              
-            previousSensorDelta = sensorDelta
-        
+                i = 1
+            if(max(rightSensor.ambient_light_intensity, leftSensor.ambient_light_intensity) < bestIntensity-12):
+                while(max(rightSensor.ambient_light_intensity, leftSensor.ambient_light_intensity) <= bestIntensity-5):
+                    adjust(i)
         motorLeft.run_forever(speed_sp=280)
         motorRight.run_forever(speed_sp=280)
 
-#8 or 10 is a good threshold since the ambient light in the lab is between 4-5
-#agressive(8)
-#fear(8)
-#love(8)
-explorer(8)
+
+# 8 or 10 is a good threshold since the ambient light in the lab is between 4-5
+# agressive(8)
+# fear(8)
+# love(8)
+explorer(3)
 
 
-
-
-def printSensorReadings():
-    while(True):
-        print("Left: " + str(leftSensor.ambient_light_intensity) + "    Right: " + str(rightSensor.ambient_light_intensity))
